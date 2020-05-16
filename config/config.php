944fc336788e85b4ac729ad757d7f1ed9fff3b3e
Pippo <46223516+Pippaccio@users.noreply.github.com>
2020-05-16 13:19:32 +0200

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'subject' => [
+        'prefix' => '[Contact Form]'
+    ],
+    'emails' => [
+        'to'   => 'filippo.lazzarin@gmail.com',
+        'from' => 'filippo.lazzarin@j4company.com'
+    ],
+    'messages' => [
+        'error'   => 'There was an error sending your message, please try again later.',
+        'success' => 'Your message has been sent successfully.'
+    ],
+    'fields' => [
+        'name'     => 'Name',
+        'email'    => 'Email',
+        'phone'    => 'Phone',
+        'subject'  => 'Subject',
+        'message'  => 'Message',
+        'btn-send' => 'Send'
+    ]
+];
